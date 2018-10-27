2b3135bc11a03bd0de4554c8c54e744e4353c257
gaiqun4726 <gaiqun4726@126.com>
2018-10-27 14:18:11 +0800

    Create E160_intersect_linked_list.java

---
+++ src/main/java/E160_intersect_linked_list.java
@@ -0,0 +1,47 @@
+public class E160_intersect_linked_list {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 1;
+        int lenB = 1;
+        while (curA.next != null) {
+            curA = curA.next;
+            lenA++;
+        }
+        while (curB.next != null) {
+            curB = curB.next;
+            lenB++;
+        }
+        if (curA != curB)
+            return null;
+        ListNode first = lenA >= lenB ? headA : headB;
+        ListNode second = lenA >= lenB ? headB : headA;
+        int step = Math.abs(lenA - lenB);
+        while (step > 0) {
+            first = first.next;
+            step--;
+        }
+        while (first != null) {
+            if (first == second)
+                break;
+            first = first.next;
+            second = second.next;
+        }
+        return first;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
