dec20bf539ba8034b74718553f4c3c8e70db381c
gaiqun4726 <gaiqun4726@126.com>
2020-02-01 11:03:50 +0800

    2020.2.1

--- src/main/java/easy/E121_Best_Stock_Profit.java
+++ src/main/java/easy/E121_Best_Stock_Profit.java
@@ -2,7 +2,7 @@ package easy;
 
 public class E121_Best_Stock_Profit {
     /**
-     * 找到极小值，以及之后最大的极大值，它们的差就是最大利润
+     * 找到最小值，以及之后的最大值，它们的差就是最大利润
      * minPrice保存最小值，maxProfit保存当前值和最小值之差的最大值
      * 每次找到比minPrice小的值则更新极小值；每次找到比minPrice大的值，则计算它和minPrice的差，并和maxPrice比较
      * 一次遍历即可求出全局最大利润
