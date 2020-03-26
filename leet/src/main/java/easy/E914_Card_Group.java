cc4c5101fcebe4068522a92bb71b35a150534110
gaiqun4726 <gaiqun4726@126.com>
2020-03-27 01:34:45 +0800

    Easy 914

--- leet/src/main/java/easy/E914_Card_Group.java
+++ leet/src/main/java/easy/E914_Card_Group.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class E914_Card_Group {
+
+    /**
+     * 暴力法，尝试所有可能整除元素个数的数字。
+     * @param deck
+     * @return
+     */
     public boolean hasGroupsSizeX(int[] deck) {
         if (deck == null || deck.length == 0)
             return false;
@@ -31,5 +37,35 @@ public class E914_Card_Group {
             }
         }
         return res;
+    }
+
+    /**
+     * 其实就是求各个卡牌个数的最大公约数是不是大于等于2.
+     * @param deck
+     * @return
+     */
+    public boolean hasGroupsSizeX2(int[] deck) {
+        if(deck==null||deck.length==0) {
+            return false;
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i: deck) {
+            if(map.containsKey(i)) {
+                map.put(i, map.get(i)+1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        int val = map.get(deck[0]);
+        for(int j: map.values()) {
+            val = gcd(j, val);
+        }
+        return val>=2;
+    }
+    int gcd(int a, int b) {
+        if(b==0) {
+            return a;
+        }
+        return gcd(b, a%b);
     }
 }
