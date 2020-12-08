39bf6e851ebe684ef8bd487f8cc498649ac834ee
gaiqun4726 <gaiqun4726@126.com>
2020-12-09 00:44:29 +0800

    20201209

--- leet/src/main/java/medium/M102_Level_Search_Binary_Tree.java
+++ leet/src/main/java/medium/M102_Level_Search_Binary_Tree.java
@@ -1,6 +1,7 @@
 package medium;
 
 import common.TreeNode;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -35,5 +36,31 @@ public class M102_Level_Search_Binary_Tree {
             lists.add(list);
         }
         return lists;
+    }
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size != 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            result.add(list);
+        }
+        return result;
     }
 }
